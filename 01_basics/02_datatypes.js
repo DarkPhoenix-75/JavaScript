3385176e1772e7be171eb351ad396cf5858f1112
DarkPhoenix-75 <sbh8257@gmail.com>
2025-09-27 07:22:55 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)  //in browser not in node
+
+let Name="hitesh"
+let age=18
+let isLoggedIn= false
+
+
+//Primitive data types
+//number  => 2^53
+//string
+//bigint
+//boolean =>true/false
+//Null  => standalone value
+//undefined  => value not assigned
+//symbol
+
+
+//user defined 
+//object
+
+console.log(typeof Name)
+console.log(typeof null)
