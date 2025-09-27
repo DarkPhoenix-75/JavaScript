4286d5614483218f80cfd885d57d19940949dd3f
DarkPhoenix-75 <sbh8257@gmail.com>
2025-09-27 08:25:16 +0000

    Comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,19 +4,32 @@
 // let score=undefined Gives NaN
 let score =true
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber) 
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
 
 // let isLoggedIn=""  //gives false
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let someNumber=33
 let stringsomenumber=String(someNumber)
-console.log(stringsomenumber)
-console.log(typeof stringsomenumber)
+// console.log(stringsomenumber)
+// console.log(typeof stringsomenumber)
+
+//   ********* Operations *********
+
+let value=3
+let negVal=-value
+console.log(negVal)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);  //true gets converted into 1
