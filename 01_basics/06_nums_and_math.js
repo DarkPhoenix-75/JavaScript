61c8940e57108fb1c2358e1c93136a8b7d14c3f5
DarkPhoenix-75 <sbh8257@gmail.com>
2025-09-28 14:49:50 +0000

    Math file added

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score =100
+
+const balance = new Number(1000)  //defining number specifically
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(score.toString())
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber=223.5326
+
+//Returns string
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds= 100000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************Maths Operator************
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.5))
+
+// console.log(Math.min(4,6,7,1,8))
+// console.log(Math.max(4,6,7,1,8))
+
+// console.log(Math.random());
+console.log(Math.floor((Math.random()*10)+1));
+console.log((Math.random()*9)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);  //To accurately get values between two numbers
