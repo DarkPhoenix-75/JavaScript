c8778c07fc68120f58f3b4929c18bf6e206f55da
Sameer <sbh8257@gmail.com>
2025-10-27 00:52:46 +0530

    File updated by me

--- 05_iterations/08.js
+++ 05_iterations/08.js
@@ -1,4 +1,4 @@
-const myNums=[1,2,3,4]
+const myNums = [1, 2, 3, 4];
 
 //Reduce: Combines all elements into a single value (sum, product, object, etc.)
 
@@ -10,25 +10,25 @@ const myNums=[1,2,3,4]
 // const newTotal=myNums.reduce((acc,curr_val)=>acc+curr_val,0);
 // console.log(newTotal);
 
-const shoppingCart=[
-    {
-        itemName:"js course",
-        price:2999
-    },
-    {
-        itemName:"py course",
-        price:1999
-    },
-    {
-        itemName:"Dsa course",
-        price:3999
-    },
-    {
-        itemName:"WEB-DEV course",
-        price:4999
-    }
-]
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 1999,
+  },
+  {
+    itemName: "Dsa course by Sameer",
+    price: 3999,
+  },
+  {
+    itemName: "WEB-DEV course",
+    price: 4999,
+  },
+];
 
-const total_price=shoppingCart.reduce((acc, item)=>(acc+item.price),0)
+const total_price = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
 console.log(total_price);
